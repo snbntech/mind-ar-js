a37f214c21a8b907a32ef75947a2f2f04e2a524d
Antares24 <antares24.email@gmail.com>
2022-07-08 15:57:35 +1000

    Added message on scanning message

--- webpack.config.prod.cjs
+++ webpack.config.prod.cjs
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'mindar-image': './src/image-target/index.js',
-    'mindar-image-aframe': './src/image-target/aframe.js',
+    // 'mindar-image': './src/image-target/index.js',
+    // 'mindar-image-aframe': './src/image-target/aframe.js',
     'mindar-image-three': './src/image-target/three.js',
-    'mindar-face': './src/face-target/index.js',
-    'mindar-face-aframe': './src/face-target/aframe.js',
-    'mindar-face-three': './src/face-target/three.js'
+    // 'mindar-face': './src/face-target/index.js',
+    // 'mindar-face-aframe': './src/face-target/aframe.js',
+    // 'mindar-face-three': './src/face-target/three.js'
   },
   mode: 'production',
   output: {
